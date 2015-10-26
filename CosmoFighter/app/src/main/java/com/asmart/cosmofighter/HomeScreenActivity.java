8f96521253491c2ddb48a6adb0e21b48376323aa
Manish Kumar <manish.vr111@gmail.com>
2015-10-25 20:18:57 -0400

    commit changes
    screens

--- CosmoFighter/app/src/main/java/com/asmart/cosmofighter/HomeScreenActivity.java
+++ CosmoFighter/app/src/main/java/com/asmart/cosmofighter/HomeScreenActivity.java
@@ -7,13 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AudioSettings extends AppCompatActivity {
+public class HomeScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_audio_settings);
-        
+
     }
 
 }
--- CosmoFighter/app/src/main/java/com/asmart/cosmofighter/Level4Activity.java
+++ CosmoFighter/app/src/main/java/com/asmart/cosmofighter/Level4Activity.java
@@ -7,13 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AudioSettings extends AppCompatActivity {
+public class Level4Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_audio_settings);
-        
+        setContentView(R.layout.activity_level4);
     }
-
 }
+
+
