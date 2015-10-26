d7595a2179d4696ffbe2db4c00f9a7aea00a3c2c
Shruthi Katapally <shruthi.katapally@gmail.com>
2015-10-25 20:02:24 -0400

    string_changes

--- CosmoFighter/app/src/main/java/com/asmart/cosmofighter/FbShare.java
+++ CosmoFighter/app/src/main/java/com/asmart/cosmofighter/FbShare.java
@@ -12,18 +12,7 @@ public class FbShare extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fb_share);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
