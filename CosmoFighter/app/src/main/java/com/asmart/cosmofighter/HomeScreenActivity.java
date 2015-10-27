83efd249938150e3b1c185dd0c446c177d0ea05a
Manish Kumar <manish.vr111@gmail.com>
2015-10-27 13:54:53 -0400

    added highscores activity

--- CosmoFighter/app/src/main/java/com/asmart/cosmofighter/HomeScreenActivity.java
+++ CosmoFighter/app/src/main/java/com/asmart/cosmofighter/HomeScreenActivity.java
@@ -8,6 +8,6 @@ public class HomeScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_levels);
     }
-
 }
--- CosmoFighter/app/src/main/java/com/asmart/cosmofighter/MainActivity.java
+++ CosmoFighter/app/src/main/java/com/asmart/cosmofighter/MainActivity.java
@@ -14,9 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
