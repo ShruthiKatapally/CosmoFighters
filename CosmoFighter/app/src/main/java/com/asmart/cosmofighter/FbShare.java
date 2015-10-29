ad0b6ad3cd3f28612b106556dc75f075ea89e992
Manish Kumar <manish.vr111@gmail.com>
2015-10-29 03:11:41 -0400

    Integrated facebook sdk and functionality to share content to fb from the app.

--- CosmoFighter/app/src/main/java/com/asmart/cosmofighter/FbShare.java
+++ CosmoFighter/app/src/main/java/com/asmart/cosmofighter/FbShare.java
@@ -1,20 +1,54 @@
 package com.asmart.cosmofighter;
-//import com.facebook.FacebookSdk;
+import com.facebook.FacebookSdk;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookSdk;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
 
+import java.nio.channels.Selector;
 
-public class FbShare extends AppCompatActivity {
-
+public class FbShare extends FragmentActivity {
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fb_share);
 
-}
+        // Initializing fb sdk
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+        // building  the content to be posted on to facebook .....!
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                    .setContentTitle("Cosmo Fighters")
+                    .setContentDescription(
+                            "Hey! Checkout our new android app. Visit the following website to download apk")
+                    .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
+                    .build();
+            shareDialog.show(linkContent);
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        setContentView(R.layout.activity_fb_share);
+        // need to create a back button here instead of fb login........
+    }
 
 }
---
+++ CosmoFighter/app/src/main/java/com/asmart/cosmofighter/MyApplication.java
@@ -0,0 +1,40 @@
+package com.asmart.cosmofighter;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MyApplication extends Application {
+
+
+    public void onCreate(){
+
+        super.onCreate();
+        printHashKey();
+    }
+
+    public void printHashKey(){
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.asmart.cosmofighter",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("ManishKumar__KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+    }
+}
