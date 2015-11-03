d5436c3f3d214327dd5ca7d93bfef1554b6ee6c3
Shruthi Katapally <shruthi.katapally@gmail.com>
2015-11-03 18:12:52 -0500

    adding missiles for the game screen

--- CosmoFighter/app/src/main/java/com/asmart/gameplay/GamePanel.java
+++ CosmoFighter/app/src/main/java/com/asmart/gameplay/GamePanel.java
@@ -3,6 +3,7 @@ package com.asmart.gameplay;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -10,13 +11,23 @@ import android.view.SurfaceView;
 
 import com.asmart.cosmofighter.R;
 
-public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     public static final int WIDTH = 1600;
     public static final int HEIGHT = 960;
     public static final int MOVESPEED = -5;
     private MainThread thread;
     private GameBackground bg;
     private GamePlayer gamePlayer;
+
+    // for missile
+    private long missilestartingtime;
+    private Random rand = new Random();
+    private ArrayList<Missile> missiles;
+
+
     public GamePanel(Context context) {
         super(context);
         getHolder().addCallback(this);
@@ -25,26 +36,34 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int format,int width, int height) {}
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+    }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder){
+    public void surfaceDestroyed(SurfaceHolder holder) {
         boolean retry = true;
-        while(retry) {
-            try{
+        int maxlooptrial = 0;
+        while (retry && maxlooptrial < 1500) {
+            maxlooptrial++;
+            try {
                 thread.setRunning(false);
                 thread.join();
-            } catch(InterruptedException e) {
+                retry = false;
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            retry = false;
+
         }
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         bg = new GameBackground(BitmapFactory.decodeResource(getResources(), R.drawable.space));
-        gamePlayer = new GamePlayer(BitmapFactory.decodeResource(getResources(),R.drawable.helicopter), 65,25,3);
+        gamePlayer = new GamePlayer(BitmapFactory.decodeResource(getResources(), R.drawable.helicopter), 65, 25, 3);
+
+        //for missile
+        missiles = new ArrayList<Missile>();
+        missilestartingtime = System.nanoTime();
 
         thread.setRunning(true);
         thread.start();
@@ -52,18 +71,15 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction()==MotionEvent.ACTION_DOWN) {
-            if (!gamePlayer.getPlaying())
-            {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (!gamePlayer.getPlaying()) {
                 gamePlayer.setPlaying(true);
-            }
-            else{
+            } else {
                 gamePlayer.setUp(true);
             }
             return true;
         }
-        if(event.getAction()==MotionEvent.ACTION_UP)
-        {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
             gamePlayer.setUp(false);
             return true;
         }
@@ -72,21 +88,68 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
     public void update() {
 
-        if(gamePlayer.getPlaying()){
+        if (gamePlayer.getPlaying()) {
             bg.update();
             gamePlayer.update();
+
+            // for missiles
+            long misslelap = (System.nanoTime() - missilestartingtime) / 1000000;
+
+            if (misslelap > (2000 - gamePlayer.getScore() / 4)) {
+                System.out.println("Reaching making missile");
+                if (missiles.size() == 0) {
+                    missiles.add(new Missile(BitmapFactory.decodeResource(getResources(), R.drawable.missile), WIDTH + 10, HEIGHT / 2, 45, 15, gamePlayer.getScore(), 13));
+                } else {
+                    missiles.add(new Missile(BitmapFactory.decodeResource(getResources(), R.drawable.missile), WIDTH + 10, (int) (rand.nextDouble() * (HEIGHT)), 45, 15, gamePlayer.getScore(), 13));
+                }
+
+                missilestartingtime = System.nanoTime();
+            }
+            for (int i = 0; i < missiles.size(); i++) {
+                missiles.get(i).update();
+
+                if (collision(missiles.get(i), gamePlayer)) {
+                    missiles.remove(i);
+                    gamePlayer.setPlaying(false);
+                    break;
+                }
+
+                if (missiles.get(i).getX() < -100) {
+                    missiles.remove(i);
+                    break;
+                }
+
+            }
         }
+    }
+
+
+    public boolean collision(GameObject a, GameObject b) {
+        
+        if (Rect.intersects(a.getRect(), b.getRect())) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void draw(Canvas canvas) {
-        final float scaleFactorX = getWidth()/(WIDTH*1.f);
-        final float scaleFactorY = getHeight()/(HEIGHT*1.f);
-        if(canvas != null) {
+        final float scaleFactorX = getWidth() / (WIDTH * 1.f);
+        final float scaleFactorY = getHeight() / (HEIGHT * 1.f);
+        if (canvas != null) {
             final int savedState = canvas.save();
             canvas.scale(scaleFactorX, scaleFactorY);
             bg.draw(canvas);
+
+            // for missile
+
+            for (Missile m : missiles) {
+                m.draw(canvas);
+            }
+
             gamePlayer.draw(canvas);
+
+
             canvas.restoreToCount(savedState);
         }
     }
---
+++ CosmoFighter/app/src/main/java/com/asmart/gameplay/Missile.java
@@ -0,0 +1,59 @@
+package com.asmart.gameplay;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.Random;
+
+public class Missile extends GameObject
+{
+    private int speed;
+    private int score;
+    private Bitmap spritesheet;
+    private Random rand = new Random();
+    private Animation animation = new Animation();
+
+    public Missile(Bitmap res,int x,int y,int w, int h, int s, int numFrames)
+    {
+        super.x = x;
+        super.y = y;
+        width = w;
+        height = h;
+        score = s;
+        // what speed a missile should come in.
+        speed = 7 + (int)(rand.nextDouble()*score/30);
+
+        if(speed > 40)
+            speed = 40;
+        Bitmap[] image = new Bitmap[numFrames];
+
+        spritesheet = res;
+
+        for(int i = 0; i < image.length; i++)
+        {
+            image[i] = Bitmap.createBitmap(spritesheet,0,i*height,width,height);
+        }
+
+        animation.setFrames(image);
+        animation.setDelay(100-speed);
+    }
+
+    public void update()
+    {
+        x-=speed;
+        animation.update();
+    }
+
+    public void draw(Canvas canvas)
+    {
+        try{canvas.drawBitmap(animation.getImage(),x,y,null);}
+        catch(Exception e) {}
+    }
+
+    @Override
+    public int getWidth()
+    {
+        return width -10;
+
+    }
+}
